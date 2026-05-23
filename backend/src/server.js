70ad32f4015913d20e8e168bfd33e56de853da2e
Suryansh Patel <suryansh30patel@gmail.com>
2026-05-24 01:23:10 +0530

    cors fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,25 +9,31 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-
     await connectDB();
 
     const server = http.createServer(app);
 
     const io = new Server(server, {
       cors: {
-        origin: "*"
-      }
+        origin: [
+          "http://localhost:5173",
+          "https://bookleaf-support-portal-eta.vercel.app",
+        ],
+        credentials: true,
+      },
     });
 
     io.on("connection", (socket) => {
       console.log("Socket Connected:", socket.id);
+
+      socket.on("disconnect", () => {
+        console.log("Socket Disconnected:", socket.id);
+      });
     });
 
     server.listen(PORT, () => {
       console.log(`Server running on ${PORT}`);
     });
-
   } catch (error) {
     console.log(error.message);
   }
